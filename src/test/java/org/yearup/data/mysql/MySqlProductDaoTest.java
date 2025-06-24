922237a8febd856e74ae02d3a3a862a4f4d8e15e
Eyob Mengistu <emengistu17@gmail.com>
2025-06-23 22:13:12 -0500

    Added ProductDao search tests for search term and price range filters

--- src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
+++ src/test/java/org/yearup/data/mysql/MySqlProductDaoTest.java
@@ -1,34 +1,31 @@
 package org.yearup.data.mysql;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.yearup.models.Product;
 
 import java.math.BigDecimal;
-import java.sql.SQLException;
+import java.util.List;
 
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
-class MySqlProductDaoTest extends BaseDaoTestClass
-{
+class MySqlProductDaoTest extends BaseDaoTestClass {
     private MySqlProductDao dao;
 
     @BeforeEach
-    public void setup()
-    {
+    public void setup() {
         dao = new MySqlProductDao(dataSource);
     }
 
     @Test
-    public void getById_shouldReturn_theCorrectProduct()
-    {
+    public void getById_shouldReturn_theCorrectProduct() {
         // arrange
         int productId = 1;
-        Product expected = new Product()
-        {{
+        Product expected = new Product() {{
             setProductId(1);
             setName("Smartphone");
             setPrice(new BigDecimal("499.99"));
@@ -47,4 +44,32 @@ class MySqlProductDaoTest extends BaseDaoTestClass
         assertEquals(expected.getPrice(), actual.getPrice(), "Because I tried to get product 1 from the database.");
     }
 
+    @Test
+    public void search_shouldReturnProductWithinPriceRange() {
+        //Arrange
+        BigDecimal min = new BigDecimal("25");
+        BigDecimal max = new BigDecimal("200");
+
+        // Act
+        List<Product> result = dao.search(null, min, max, null, null);
+
+        // Assert
+        assertFalse(result.isEmpty(), "Expected products within the price range 25 to 200");
+        assertTrue(result.stream().allMatch(p -> p.getPrice().compareTo(min) >= 0 && p.getPrice().compareTo(max) <= 0),
+                "All product prices should fall within range.");
+    }
+
+    @Test
+    public void search_shouldReturnProductsMatchingSearchTerm() {
+        // arrange
+        String searchTerm = "phone";
+        // act
+        List<Product> result = dao.search(null, null, null, null, searchTerm);
+        // assert
+        assertFalse(result.isEmpty(), "Expected products matching 'phone'.");
+        assertTrue(result.stream().anyMatch(p ->
+                        p.getName().toLowerCase().contains("phone") ||
+                                p.getDescription().toLowerCase().contains("phone")),
+                "Expected product name or description to contain 'phone'");
+    }
 }
